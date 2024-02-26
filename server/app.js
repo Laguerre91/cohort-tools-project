ce922edfcede2fd460a7859f8d0ec7324aed7752
Jeronimo Olea <jerooleag2004@gmail.com>
2024-02-26 13:27:16 +0100

    server and package-lock changed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -29,6 +28,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get('/api/cohorts', (req, res) => {              
+  res.sendFile(`${__dirname}/cohorts.json`)
+})
+
+app.get('/api/students', (req, res) => {              
+  res.sendFile(`${__dirname}/students.json`)
+})
 
 // START SERVER
 app.listen(PORT, () => {
